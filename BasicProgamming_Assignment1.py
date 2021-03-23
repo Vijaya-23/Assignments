760ea5580ffa1040151423b3833d222bff81bbde
Vijaya-23 <76451479+Vijaya-23@users.noreply.github.com>
2021-03-23 18:13:29 +0530

    progamming assignment

---
+++ BasicProgamming_Assignment1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Q1
+print("Hello Python")
+
+
+# In[6]:
+
+
+#Q2
+a = 4
+b = 2
+x = a + b
+y = a/b
+print("addition and multiplication of a and b is", x ,y)
+
+
+# In[7]:
+
+
+#Q3 area of triangle=1/2.base.height
+base = 5
+height =10
+Area = (1/2)*base*height
+print("Area of the triangle is", Area)
+
+
+# In[10]:
+
+
+#Q4 swapping of numbers
+a = input("first number is",)
+b = input("second number is",)
+swap = a
+a = b
+b = swap
+print("Numbers after swapping ", a, b)
+
+
+# In[4]:
+
+
+#Q5 Generating a random number
+import random
+value = random.randint(0,10)
+print(value)
+
+
+# In[ ]:
+
+
+
+
